f937c3927f79eaa8eaca56211d7c64e4e401d71f
Jan-Christiaan Basson <jan.basson@gmail.com>
2017-11-02 12:16:18 +0000

    Update landMarkRemarkContainer.js

--- client/components/landMarkRemark/view/landMarkRemarkContainer.js
+++ client/components/landMarkRemark/view/landMarkRemarkContainer.js
@@ -1,18 +1,7 @@
 import React, {Component} from 'react'
-//import {} from '../actions/appActions'
 import LandMarkRemarkPresenter from './LandMarkRemarkPresenter'
 
 class LandMarkRemarkContainer extends Component {
-    constructor(props) {
-        super(props)
-    }
-
-    componentDidMount() {
-    }
-
-    componentDidUpdate(prevProps) {
-    }
-
     render() {
         return LandMarkRemarkPresenter(this.props);
     }
