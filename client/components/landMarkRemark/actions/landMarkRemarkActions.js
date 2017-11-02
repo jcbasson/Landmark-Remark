f0e38e99a926c5d3b98d5547132bbe6187a29976
Jan-Christiaan Basson <jan.basson@gmail.com>
2017-11-02 15:26:05 +0000

    Built in ability to update remarks

--- client/components/landMarkRemark/actions/landMarkRemarkActions.js
+++ client/components/landMarkRemark/actions/landMarkRemarkActions.js
@@ -1,16 +1,30 @@
 import {
-    UPDATE_LANDMARK_FOCUSED_ON
+    UPDATE_LANDMARK_FOCUSED_ON,
+    UPDATE_LANDMARK_REMARK
 } from '../constants/actionTypes'
 
 /**
- * @param landMarkId
- * @param hasFocus
- * @returns {{type, landMarkId: *, hasFocus: *}}
+ * @param <Integer> landMarkId
+ * @param <Boolean> hasFocus
+ * @returns {{type: String, landMarkId: Integer, hasFocus: Boolean}}
  */
 export const updateLandMarkHasFocus = (landMarkId, hasFocus) => {
     return {
         type: UPDATE_LANDMARK_FOCUSED_ON,
         landMarkId,
         hasFocus
+    }
+};
+
+/**
+ * @param <Integer> landMarkId
+ * @param <String> remarkText
+ * @returns {{type: String, landMarkId: Integer, remarkText: String}}
+ */
+export const updateRemark = (landMarkId, remarkText) => {
+    return {
+        type: UPDATE_LANDMARK_REMARK,
+        landMarkId,
+        remarkText
     }
 };
